19eeb61e99692ad24780fa61660173099be773f5
rodriguezcravero <nacho.rodriguez85@gmail.com>
2020-08-11 11:25:40 -0300

    portfolio primer commit

---
+++ app.js
@@ -0,0 +1,103 @@
+let textoBio1 = document.querySelector(".textoBio1");
+let textoBio2 = document.querySelector(".textoBio2");
+let textoBio3 = document.querySelector(".textoBio3");
+
+let descargaCV = document.querySelector(".descargaCV");
+
+descargaCV.addEventListener("click", descargar);
+
+function descargar() {
+  var link = document.createElement("a");
+  link.href = url;
+  link.download =
+    "https://cdn.cienradios.com/wp-content/uploads/sites/3/2019/12/Screenshot_1-5-322x400.jpg";
+  link.dispatchEvent(new MouseEvent("click"));
+}
+
+let puntosSuspensivos1 = document.querySelector(".puntosSuspensivos1");
+let puntosSuspensivos2 = document.querySelector(".puntosSuspensivos2");
+
+setInterval(() => {
+  setTimeout(() => (puntosSuspensivos1.innerHTML = ""), 500);
+  setTimeout(() => (puntosSuspensivos2.innerHTML = ""), 500);
+  setTimeout(() => (puntosSuspensivos1.innerHTML = "."), 1000);
+  setTimeout(() => (puntosSuspensivos2.innerHTML = "."), 1000);
+  setTimeout(() => (puntosSuspensivos1.innerHTML = ".."), 1500);
+  setTimeout(() => (puntosSuspensivos2.innerHTML = ".."), 1500);
+  setTimeout(() => (puntosSuspensivos1.innerHTML = "..."), 2000);
+  setTimeout(() => (puntosSuspensivos2.innerHTML = "..."), 2000);
+}, 4000);
+
+setTimeout(() => (textoBio1.innerHTML = "E"), 100);
+setTimeout(() => (textoBio1.innerHTML = "Es"), 200);
+setTimeout(() => (textoBio1.innerHTML = "Est"), 300);
+setTimeout(() => (textoBio1.innerHTML = "Estu"), 400);
+setTimeout(() => (textoBio1.innerHTML = "Estud"), 500);
+setTimeout(() => (textoBio1.innerHTML = "Estudi"), 600);
+setTimeout(() => (textoBio1.innerHTML = "Estudia"), 700);
+setTimeout(() => (textoBio1.innerHTML = "Estudian"), 800);
+setTimeout(() => (textoBio1.innerHTML = "Estudiant"), 900);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante"), 1000);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante d"), 1100);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de "), 1200);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de I"), 1300);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de In"), 1400);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Inf"), 1500);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Info"), 1600);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Infor"), 1700);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Inform"), 1800);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Informa"), 1900);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Inform"), 2000);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Informá"), 2100);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Informát"), 2200);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Informáti"), 2300);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Informátic"), 2400);
+setTimeout(() => (textoBio1.innerHTML = "Estudiante de Informática"), 2500);
+
+setTimeout(() => (textoBio2.innerHTML = "UNLP - 2º año"), 2800);
+
+setTimeout(() => (textoBio3.innerHTML = "E"), 3000);
+setTimeout(() => (textoBio3.innerHTML = "Ex"), 3100);
+setTimeout(() => (textoBio3.innerHTML = "Exp"), 3200);
+setTimeout(() => (textoBio3.innerHTML = "Expe"), 3300);
+setTimeout(() => (textoBio3.innerHTML = "Exper"), 3400);
+setTimeout(() => (textoBio3.innerHTML = "Experi"), 3500);
+setTimeout(() => (textoBio3.innerHTML = "Experie"), 3600);
+setTimeout(() => (textoBio3.innerHTML = "Experien"), 3700);
+setTimeout(() => (textoBio3.innerHTML = "Experienc"), 3800);
+setTimeout(() => (textoBio3.innerHTML = "Experienci"), 3900);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia"), 4000);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: "), 4100);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: e"), 4200);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: ex"), 4300);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: exp"), 4400);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: expe"), 4500);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: exper"), 4600);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: expert"), 4700);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: experto"), 4800);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: expert"), 5300);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: exper"), 5400);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: expe"), 5500);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: exp"), 5600);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: ex"), 5700);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: "), 5800);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: j"), 5900);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: ju"), 6000);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: jun"), 6100);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: juni"), 6200);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: junio"), 6300);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: junior"), 6400);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: junio"), 6800);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: jun"), 6900);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: ju"), 7000);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: j"), 7100);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: "), 7200);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: t"), 7300);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: tr"), 7400);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: tra"), 7500);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: trai"), 7600);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: train"), 7700);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: traine"), 7800);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: trainee"), 7900);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: traineè"), 8200);
+setTimeout(() => (textoBio3.innerHTML = "Experiencia: traineé"), 8300);
